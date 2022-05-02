e5497c10aed15957369e579e97bee5497f0590d2
gilskie <gilskie1989@gmail.com>
2022-05-02 16:08:00 +0800

    additional tutorials and learnings

--- resources/js/components/FollowButton.vue
+++ resources/js/components/FollowButton.vue
@@ -3,6 +3,7 @@
     <button
       class="btn btn-primary btn-sm"
       style="margin-left: 15px; padding: 5px 10px"
+      @click="followUser"
     >
       Follow
     </button>
@@ -11,8 +12,23 @@
 
 <script>
 export default {
+  props: ["userId"],
+
   mounted() {
     console.log("Component mounted.");
+  },
+
+  methods: {
+    followUser() {
+      axios
+        .post("/follow/" + this.userId)
+        .then((response) => {
+          alert(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
   },
 };
 </script>
